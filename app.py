d7b2a5d515ae86775bb67201062f81c019429295
Intel-Unnati @saintgits <157574604+intel-unnati-saintgits@users.noreply.github.com>
2024-02-22 20:31:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ if st.button("Predict type of Iris"):
 st.text('')
 st.text('')
 st.markdown(
-    '`Create by` [Intel-Unnati](https://sijuswamy.github.io/mywebsite/) | \
-         `Code:` [GitHub](https://github.com/sijuswamy)')
+    '`Create by` [Intel-Unnati](https://github.com/intel-unnati-saintgits/intel-unnati-certificate-programme/blob/main/README.md) | \
+         `Code:` [GitHub](https://github.com/intel-unnati-saintgits/Model-Deployment-Demo/edit/main/app.py)')
